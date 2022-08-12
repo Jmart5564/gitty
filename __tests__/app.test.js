ff73d2bbcf2e3e80dfbccce26f222927d14a1752
Jmart5564 <Jmart5564@gmail.com>
2022-08-12 16:44:05 -0700

    got my first test to pass!

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,8 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
+// const agent = request.agent(app);
 jest.mock('../lib/services/github');
 
 describe('backend-express-template routes', () => {
@@ -12,21 +13,22 @@ describe('backend-express-template routes', () => {
     expect(1).toEqual(1);
   });
   
-  // it('should login and redirect users to /api/v1/github/dashboard', async () => {
-  //   const res = await request
-  //     .agent(app)
-  //     .get('/api/v1/github/callback?code=42')
-  //     .redirects(1);
+  it('should login and redirect users to /api/v1/github/dashboard', async () => {
+    const res = await request
+      .agent(app)
+      .get('/api/v1/github/callback?code=42')
+      .redirects(1);
 
-  //   expect(res.body).toEqual({
-  //     id: expect.any(String),
-  //     username: 'user',
-  //     email: 'user@me.com',
-  //     avatar: expect.any(String),
-  //     iat: expect.any(Number),
-  //     exp: expect.any(Number),
-  //   });
-  // });
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      username: 'user',
+      email: 'user@me.com',
+      avatar: expect.any(String),
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+    });
+  });
+
 
   afterAll(() => {
     pool.end();
